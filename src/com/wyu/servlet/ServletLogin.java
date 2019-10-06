2a563ecaaceb0e44a63f579c1878dc34eee2d9fc
MrChen01 <37013572+MrChen01@users.noreply.github.com>
2019-10-06 17:22:38 +0800

    /增加servlet层 login test 的修复dopost()方法

--- src/com/wyu/servlet/ServletLogin.java
+++ src/com/wyu/servlet/ServletLogin.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 @WebServlet("/loginServlet")
 public class ServletLogin extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
