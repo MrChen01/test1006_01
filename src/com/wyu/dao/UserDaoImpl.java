3774ece3981db4302d2685deb2d3deecdeef4d39
MrChen01 <37013572+MrChen01@users.noreply.github.com>
2019-10-06 15:45:49 +0800

    //增加Dao实现类 UserDaoImpl

---
+++ src/com/wyu/dao/UserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.wyu.dao;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public int findIdByName(String name) {
+        return 0;
+    }
+
+    @Override
+    public boolean login() {
+        return false;
+    }
+}
