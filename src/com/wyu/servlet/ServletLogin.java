c1f3bb293a966693094c1a504966d2515c05322d
MrChen01 <37013572+MrChen01@users.noreply.github.com>
2019-10-06 17:08:25 +0800

    /增加servlet层 login test

---
+++ src/com/wyu/servlet/ServletLogin.java
@@ -0,0 +1,15 @@
+package com.wyu.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/loginServlet")
+public class ServletLogin extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("servlet");
+    }
+}
